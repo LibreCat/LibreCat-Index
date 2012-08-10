02b02ac62fc215f3e81e6824317e40388fa345b1
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2012-08-10 16:03:55 +0200

    Fixing collision

--- bin/index_collection.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for db in rug01 rug02 rug03 dbs01 bkt01 ebk01 pug01 ser01 ejn01; do
-  if [ $db == "ejn01" ]; then
-    zcat /vol/indexes/incoming/${db}.export.gz | ./marcxml_aleph.pl - | ./meercat_index.pl -v --clear ${db} -
-  else
-    zcat /vol/indexes/incoming/${db}.export.gz | ./meercat_index.pl -v --clear ${db} -
-  fi
-done
